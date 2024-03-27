e95bc5e62576677ff111ea944845354d2c409f2d
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-27 16:47:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -27,35 +27,41 @@ final class Main {
     * @param args No args will be used
     */
     public static void main(String[] args) {
+        // constants
+        final String fruits = "fruits: ";
+        final String popped = "popped: ";
+        final String colours = "colours: ";
+        final String shapes = "shapes: ";
+        
         final CurtisStack fruits = new CurtisStack();
         fruits.push("apple");
         fruits.push("banana");
         fruits.push("cherry");
-        System.out.println("fruits: " + fruits.showStack());
-        System.out.println("popped: " + fruits.popItem());
-        System.out.println("fruits: " + fruits.showStack());
+        System.out.println(fruits + fruits.showStack());
+        System.out.println(popped + fruits.popItem());
+        System.out.println(fruits + fruits.showStack());
         System.out.println("");
 
         final CurtisStack colours = new CurtisStack();
         colours.push("red");
         colours.push("green");
         colours.push("blue");
-        System.out.println("colours: " + colours.showStack());
-        System.out.println("popped: " + colours.popItem());
-        System.out.println("popped: " + colours.popItem());
-        System.out.println("colours: " + colours.showStack());
+        System.out.println(colours + colours.showStack());
+        System.out.println(popped + colours.popItem());
+        System.out.println(popped + colours.popItem());
+        System.out.println(colours + colours.showStack());
         System.out.println("");
 
         final CurtisStack shapes = new CurtisStack();
         shapes.push("square");
         shapes.push("triangle");
         shapes.push("circle");
-        System.out.println("shapes: " + shapes.showStack());
-        System.out.println("popped: " + shapes.popItem());
-        System.out.println("popped: " + shapes.popItem());
-        System.out.println("popped: " + shapes.popItem());
-        System.out.println("popped: " + shapes.popItem());
-        System.out.println("shapes: " + shapes.showStack());
+        System.out.println(shapes + shapes.showStack());
+        System.out.println(popped + shapes.popItem());
+        System.out.println(popped + shapes.popItem());
+        System.out.println(popped + shapes.popItem());
+        System.out.println(popped + shapes.popItem());
+        System.out.println(shapes + shapes.showStack());
 
         System.out.println("\nDone.");
     }
