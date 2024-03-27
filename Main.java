e6bfd93a9b2288fbcce4ee38d4be8cb090058eb7
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-27 16:18:51 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -32,17 +32,27 @@ final class Main {
         fruits.push("banana");
         fruits.push("cherry");
         System.out.println("fruits: " + fruits.showStack());
+        System.out.println("popped: " + fruits.popItem());
+        System.out.println("fruits: " + fruits.showStack());
 
         final CurtisStack<String> colours = new CurtisStack<>();
         colours.push("red");
         colours.push("green");
         colours.push("blue");
         System.out.println("colours: " + colours.showStack());
+        System.out.println("popped: " + colours.popItem());
+        System.out.println("popped: " + colours.popItem());
+        System.out.println("colours: " + colours.showStack());
 
         final CurtisStack<String> shapes = new CurtisStack<>();
         shapes.push("square");
         shapes.push("triangle");
         shapes.push("circle");
+        System.out.println("shapes: " + shapes.showStack());
+        System.out.println("popped: " + shapes.popItem());
+        System.out.println("popped: " + shapes.popItem());
+        System.out.println("popped: " + shapes.popItem());
+        System.out.println("popped: " + shapes.popItem());
         System.out.println("shapes: " + shapes.showStack());
 
         System.out.println("\nDone.");
